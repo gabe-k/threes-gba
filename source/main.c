6ebd29ab65cb4d58571c7a4293b7e6ea5c6a668b
Gabe Kirkpatrick <g@be-k.biz>
2023-03-05 23:17:02 -0800

    started implementing deck functions

--- source/main.c
+++ source/main.c
@@ -1,15 +1,15 @@
-
 #include <tonc_core.h>
 #include <tonc_memmap.h>
 #include <tonc_oam.h>
 #include <tonc_input.h>
 #include <tonc_video.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "sprites.h"
 
 typedef enum _game_tile {
-	empty = -1,
+	empty,
 	tile_1,
 	tile_2,
 	tile_3,
@@ -29,8 +29,54 @@ typedef enum _game_tile {
 
 char board[4][4];
 
+char start_deck[] = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+char current_deck[12];
+
+bool is_deck_empty(char* deck) {
+	return deck[0] == 0;
+}
+
+void clear_deck(char* deck, int len) {
+	int c = 0;
+	for(int i = 0; i < len; i++) {
+		if (deck[i] == 0) {
+			continue;
+		}
+		deck[c] = deck[i];
+		c++;
+	}
+	memset(deck+c, 0, len - c);
+}
+
+void shuffle_deck(char* deck, int len) {
+	int flips = qran_range(len / 2, len);
+	for (int i = 0; i < flips; i++) {
+		int i1 = qran_range(0, len);
+		int i2 = qran_range(0, len);
+		int v1 = deck[i1];
+		int v2 = deck[i2];
+		deck[i2] = v1;
+		deck[i1] = v2;
+	}
+}
+
 game_tile random_game_tile() {
-	return qran_range(tile_1, tile_6);
+	game_tile cur_tile = empty;
+
+	// if the deck is empty then bring in the starter deck and shuffle it
+	if (is_deck_empty(current_deck))
+	 {
+		memcpy(current_deck, start_deck, sizeof(current_deck));
+		shuffle_deck(current_deck, sizeof(current_deck));
+	}
+
+	// take the first
+	cur_tile = current_deck[0];
+	current_deck[0] = 0;
+	clear_deck(current_deck, sizeof(current_deck));
+
+
+	return cur_tile;
 }
 
 void oam_init(OBJ_ATTR *obj, uint count)
@@ -60,14 +106,13 @@ typedef struct _POS2D {
 void draw_board() {
 	for(int x = 0; x < 4; x++) {
 		for (int y = 0; y < 4; y++) {
-			if ((u8)(board[x][y])
-			 == (u8)empty) {
+			if ((u8)(board[x][y]) == (u8)empty) {
 				obj_hide(&test_objs[(y * 4) + x]);
 				continue;
 			}
 
-			obj_set_attr(&test_objs[(y * 4) + x], ATTR0_SQUARE | ATTR0_8BPP | ATTR0_REG, ATTR1_SIZE_32, ATTR2_PALBANK(0) | (board[x][y] * 32) | ATTR2_PRIO(1));
-			obj_set_pos(&test_objs[(y * 4) + x], 50 + (x * 27), 32 + (y * 32));
+			obj_set_attr(&test_objs[(y * 4) + x], ATTR0_SQUARE | ATTR0_8BPP | ATTR0_REG, ATTR1_SIZE_32, ATTR2_PALBANK(0) | ((board[x][y] - 1) * 32) | ATTR2_PRIO(1));
+			obj_set_pos(&test_objs[(y * 4) + x], 50 + (x * 27), 20 + (y * 32));
 		}
 	}
 
@@ -75,7 +120,8 @@ void draw_board() {
 }
 
 void reset_board() {
-	memset(board, 0xff, sizeof(board));
+	memset(current_deck, 0, sizeof(current_deck));
+	memset(board, 0, sizeof(board));
 
 	for (int i = 0; i < 9; i++) {
 		int cx, cy;
@@ -97,6 +143,8 @@ int main() {
 
 	oam_init(test_objs, 128);
 	REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D;
+
+	reset_board();
 
 	while(1) {
 		vid_vsync();
